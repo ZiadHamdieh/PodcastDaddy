2372e977ddeb672f9e73f6ecb7d7c08a3a37fd17
Ziad Hamdieh <z.a.hamdieh@gmail.com>
2019-03-24 18:47:25 -0400

    added tab bar controller

---
+++ PodcastDaddy/PodcastDaddy/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  PodcastDaddy
+//
+//  Created by Ziad Hamdieh on 2019-03-24.
+//  Copyright © 2019 Ziad Hamdieh. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .purple
+        view.backgroundColor = .white
+        
+        viewControllers = [
+            generateNavigationController(for: UIViewController(), title: "Favourites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: UIViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: UIViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    fileprivate func generateNavigationController(for viewController: UIViewController, title: String, image: UIImage) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: viewController)
+        viewController.navigationItem.title = title
+        navController.navigationBar.prefersLargeTitles = true
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+    
+}
